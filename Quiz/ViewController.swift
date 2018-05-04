d943c9082486d53d28e8c6367708836e64fae41f
Radoslav Bonev <radoslav.bonev@netea.bg>
2018-05-05 01:17:57 +0300

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Radoslav Bonev on 5/5/18.
+//  Copyright © 2018 Radoslav Bonev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
